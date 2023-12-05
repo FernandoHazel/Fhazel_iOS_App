2f13da99cba7eb9373a296d9f39b8c7417db52c1
Fernando Hazel <fernandohazel1@gmail.com>
2023-12-05 16:55:04 -0600

    Initial Commit

---
+++ FhazeliOSApp/FhazeliOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FhazeliOSAppApp.swift
+//  FhazeliOSApp
+//
+//  Created by gdaalumno on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FhazeliOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
